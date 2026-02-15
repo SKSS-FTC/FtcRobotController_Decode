fc326b568c4be7ba5ee1a7161187fed997500a2c
User <skss.skyrocket@gmail.com>
2026-02-15 23:31:16 +0800

    Add shooterAlgorithmTest1 OpMode for testing BlueShooter PID/SMC algorithms

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/shooterAlgorithmTest1.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystem.shooter.BlueShooter;
+
+@TeleOp(name = "ShooterAlgorithmTest1", group = "Tests")
+public class shooterAlgorithmTest1 extends LinearOpMode {
+    private BlueShooter blueShooter;
+
+    @Override
+    public void runOpMode() {
+        blueShooter = new BlueShooter(hardwareMap);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.triangle) {
+                blueShooter.shooterAiming = true;
+            }
+            if (gamepad1.cross) {
+                blueShooter.shooterAiming = false;
+            }
+            blueShooter.update(new Pose(0,0),120);
+            telemetry.addData("Shooter Aiming", blueShooter.shooterAiming);
+            telemetry.update();
+        }
+    }
+}
